56cca9a433ba54a638da57052f497a4e30b613f0
BZHan <40694153+Uritard@users.noreply.github.com>
2022-12-07 23:31:45 -0500

    Update mapdialog.cpp

--- mapping-my-trip-main/mapdialog.cpp
+++ mapping-my-trip-main/mapdialog.cpp
@@ -149,9 +149,9 @@ void MapDialog::on_searchBotton_clicked() // search place on a map
 
 void MapDialog::on_pathButton_clicked()  //open path finding ui
 {
-    pathDialog pathD;
-    pathD.setModal(true);
-    pathD.exec();
+    pathDialog *pathd = new pathDialog;
+    pathd->setModal(true);
+    pathd->exec();
 }
 
 string MapDialog::p_name(){
